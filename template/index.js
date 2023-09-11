cc10e53a75294fdea61e71fe99d94552b41e07f6
Capy <137941226+CapyKing10@users.noreply.github.com>
2023-09-11 15:17:42 +0200

    Update index.js

--- template/index.js
+++ template/index.js
@@ -9,9 +9,9 @@ process.on('warning', e => console.warn(e.stack));
 
 const main = () => {
   bot = mineflayer.createBot({
-    host: "anarchy.6b6t.org",
+    host: "config.ip",
     username: config.username,
-    version: "1.19.4",
+    version: "config.version",
     skipValidation: true,
   });
 
